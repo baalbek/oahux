b103ca5d5f4b5f0020881578a3fb928aef6ac692
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2013-09-16 17:17:36 +0200

    StockPriceFx

---
+++ src/main/java/oahux/domain/StockPriceFx.java
@@ -0,0 +1,17 @@
+package oahux.domain;
+
+import javafx.beans.property.DoubleProperty;
+import oahu.financial.StockPrice;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: rcs
+ * Date: 9/16/13
+ * Time: 10:30 AM
+ */
+public interface StockPriceFx extends StockPrice {
+    DoubleProperty opnProperty();
+    DoubleProperty hiProperty();
+    DoubleProperty loProperty();
+    DoubleProperty clsProperty();
+}
