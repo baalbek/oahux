0f36c5d95aa2bb7b0f0d94d82f9e418118d0c2a1
rcs <rolf.c.stadheim@gmail.com>
2014-10-12 23:48:14 +0200

    Maunaloa StockMarketRepository, Ranoraraku selectStockPrices impl in StockMarketReposImpl

--- src/main/java/oahux/financial/DerivativeFx.java
+++ src/main/java/oahux/financial/DerivativeFx.java
@@ -3,6 +3,7 @@ package oahux.financial;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.DoubleProperty;
 import oahu.financial.Derivative;
+import oahu.financial.DerivativePrice;
 
 /**
  * Created with IntelliJ IDEA.
@@ -10,7 +11,7 @@ import oahu.financial.Derivative;
  * Date: 8/1/13
  * Time: 9:20 AM
  */
-public interface DerivativeFx extends Derivative {
+public interface DerivativeFx extends DerivativePrice {
     BooleanProperty isCheckedProperty();
     DoubleProperty riskProperty();
     void setRisk(double value);
