c378ba26c9c4482792ee4df271bb02544b47f2ae
rcs <rolf.c.stadheim@gmail.com>
2015-08-23 18:17:38 +0200

    ControllerEnum wherever old int location was used

--- src/main/java/oahux/controllers/ControllerEnum.java
+++ src/main/java/oahux/controllers/ControllerEnum.java
@@ -6,13 +6,4 @@ package oahux.controllers;
  */
 public enum ControllerEnum {
     EMPTY, DAY, WEEK, OSEBX_DAY, OSEBX_WEEK;
-    public static ControllerEnum fromInt(int i) {
-        switch (i) {
-            case 2: return DAY;
-            case 3: return WEEK;
-            case 4: return OSEBX_DAY;
-            case 5: return OSEBX_WEEK;
-            default: return EMPTY;
-        }
-    }
 }
