bd8779e22fac754fd64580f1a7506df8d49da3e9
Rolf C Stadheim <rolf.c.stadheim@gmail.com>
2016-01-22 13:01:32 +0100

    ChartItemRepository refactoring

--- src/main/java/oahux/controllers/ControllerLocation.java
+++ src/main/java/oahux/controllers/ControllerLocation.java
@@ -4,6 +4,6 @@ package oahux.controllers;
  * Created by rcs on 23.08.15.
  *
  */
-public enum ControllerCategory {
+public enum ControllerLocation {
     EMPTY, DAY, WEEK, OSEBX_DAY, OSEBX_WEEK;
 }
